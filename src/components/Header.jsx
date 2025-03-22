26cc9b00f1f3c7bd785bf902fbea1fe8b4cf7e47
Akash Jape <ajjape01@gmail.com>
2025-03-22 20:02:21 +0530

    footer change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useRef } from 'react'
 import { PostList } from './store/PostListContext';
+import logo from '../assets/connectify.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const { setSearchQuery } = useContext(PostList);
@@ -8,7 +10,13 @@ const Header = () => {
 
   return (
     <>
-    <header className="p-3 border-bottom heading">
+    <header className="p-3 border-bottom ">
+    <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none headingInApp ">
+      <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
+     
+      <span className="fs-4"> <img src={logo} alt="logo"  className='logo' />Connectify</span>
+    </Link>
+
     <div className="container headContainer">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start headContainer">
         <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
